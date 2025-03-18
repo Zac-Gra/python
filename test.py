d5edd383161aad98cfa7cac462d45bd3b360e41c
Zac <lilshicox@gmail.com>
2025-03-19 10:11:30 +1300

    finish i think

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 cat_list = []
 loop_count = int(1)
 while loop_count < 4:
-    cat_name = input (f"Enter the name of a cat")
+    cat_name = input (f"Enter the name of a cat? ")
+    cat_list.append(cat_name) 
+    loop_count = loop_count + 1
+print( cat_list )
