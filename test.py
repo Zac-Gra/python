bc88061093cc762c824a0246203cb2ec89ad33c4
Zac-Gra <lilshicox@gmail.com>
2025-04-03 11:59:56 +1300

    added more print statements

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-cat_list = []
-loop_count = int(1)
-while loop_count < 4:
-    cat_name = input (f"Enter the name of a cat? ")
-    cat_list.append(cat_name) 
-    loop_count = loop_count + 1
-print( cat_list )
+Name = input ("What is your name: ") 
+Age = input ("What is your age: ") 
+
+print ("Choose an activity: ")
+print ("option 1")
+
+print ("1. Music Jam Session (2 hours, easy, $5 fee)")
+print ("2. Science Experiments lab (3 hours, moderate, $10 fee)")
+print ("3. Sports Leadership Training (4 hours, challenging, $12 fee)")
+Activity = input ("Enter the number of your chosen activity: ")
