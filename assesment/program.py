4300615d47f0d857055cff064416ec3f35bb64e2
Zac-Gra <lilshicox@gmail.com>
2025-05-01 12:09:00 +1200

    added print and input for name and age

--- assesment/program.py
+++ assesment/program.py
@@ -1,1 +1,8 @@
-print ("hello")
+print ("Welcome to the camp sign up page please answer these qustions truthfuly otherwise the bro ryser gonna bash you")
+Name = input("What is your name: ")
+Age = int(input("What is your age: "))
+
+if Age < 17:
+    print("Too young to go")
+else:
+    print("You're old enough")
