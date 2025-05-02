2c6e9b80cb1b01e81fd3e5266349af4dc29dbb6b
Zac-Gra <lilshicox@gmail.com>
2025-05-02 13:13:30 +1200

    added a working activity selection

--- assesment/program.py
+++ assesment/program.py
@@ -9,3 +9,14 @@ elif Age >= 6 and Age <= 16:
     print("You're allowed to go (yay) ")
 else:
     print("not a number goofy ")
+
+print ("Chosse an activity to do: \n 1. Cultural immersion. This is for 5 days and is considered “easy” and costs $800.  \n 2. Kayaking and pancakes. This is for 3 days and is considered “moderate” and costs $ 400.  \n 3.Mountain biking. This is for 4 days and is considered “difficult” and costs $900")
+Activity = int(input("What would you like to do: "))
+if Activity == 1:
+    print("Your going to cultural immersion")
+elif Activity == 2:
+    print("Your going to kayaking and pancakes")
+elif Activity == 3:
+    print("Your going to mountain biking")
+else:
+    print("not a number goofy aahh ")
