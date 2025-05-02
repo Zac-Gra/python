dbcd324997925837026cf781a5acb3640a7c4dc4
Zac-Gra <lilshicox@gmail.com>
2025-05-02 12:54:31 +1200

    trying to fix code

--- assesment/program.py
+++ assesment/program.py
@@ -1,8 +1,9 @@
 print ("Welcome to the camp sign up page please answer these qustions truthfuly otherwise the bro ryser gonna bash you")
 Name = input("What is your name: ")
 Age = int(input("What is your age: "))
-
-if Age < 17:
-    print("Too young to go")
-else:
-    print("You're old enough")
+if Age > 17:
+    print("Your not algs to go")
+if Age < 5:
+    print("You're not old enough")
+if Age == [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]:
+    print("your algs to go")
